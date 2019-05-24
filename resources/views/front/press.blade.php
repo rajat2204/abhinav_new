166326fb3106cb40ac540d915500e519d9501167
Raghvendra Singh <raghvendra.igniterpro@gmail.com>
2019-05-24 10:51:03 +0530

    changes saved

---
+++ resources/views/front/press.blade.php
@@ -0,0 +1,13 @@
+
+
+<div id="press-page">
+    {{-- press page header section  --}}
+    <div class="press-page-header">
+        <div class="press-page-heading-image">
+            <img src="{{asset('assets/img/Union 42.png')}}"  alt="union 42 image not available" >
+        </div>
+        <div class="press-page-hero-heading">
+            Press
+        </div>
+    </div>
+</div>
