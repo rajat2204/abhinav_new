2fd8fc65793bababbceb29248cd66d5151755ac8
Raghvendra Singh <raghvendra.igniterpro@gmail.com>
2019-05-24 11:34:09 +0530

    chages saved

--- resources/views/front/press.blade.php
+++ resources/views/front/press.blade.php
@@ -4,10 +4,46 @@
     {{-- press page header section  --}}
     <div class="press-page-header">
         <div class="press-page-heading-image">
-            <img src="{{asset('assets/img/Union 42.png')}}"  alt="union 42 image not available" >
+            <img src="{{asset('assets/img/Union 42.png')}}"  alt="union 42 image not available">
         </div>
         <div class="press-page-hero-heading">
             Press
         </div>
     </div>
+    <div class="press-content-section">
+        
+        <div class="press-wrapper">
+                <div class="blog-card">
+            
+                        <div class="blog-card-image">
+                            <img src="{{asset('assets/img/Beaches-in-India_1.jpg')}}" alt="image not found" >
+                        </div> 
+                        <div class="blog-card-heading">
+                            Source Heading shows here.. 
+                        </div>
+                        <div class="blog-card-content">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem, error, atque, magnam dicta voluptates autem reprehenderit facilis quo rem nam nisi. Error magnam nam ab maiores mollitia corporis quas. Suscipit.  
+                         </div> 
+                         <div class="link-wrapper">
+                                <a href="" class="read-more">Source</a>
+                            </div>        
+                    </div>
+                    <div class="blog-card">
+            
+                            <div class="blog-card-image">
+                                <img src="{{asset('assets/img/Beaches-in-India_1.jpg')}}" alt="image not found" >
+                            </div> 
+                            <div class="blog-card-heading">
+                                Source Heading shows here.. 
+                            </div>
+                            <div class="blog-card-content">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem, error, atque, magnam dicta voluptates autem reprehenderit facilis quo rem nam nisi. Error magnam nam ab maiores mollitia corporis quas. Suscipit.  
+                             </div> 
+                             <div class="link-wrapper">
+                                    <a href="" class="read-more">Source</a>
+                                </div>        
+                        </div>
+        </div>
+    </div>
+
 </div>
