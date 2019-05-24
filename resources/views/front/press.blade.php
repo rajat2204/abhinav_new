fc0a143c37cb031ad616248271058b411ebf6d88
rajat <rajat.igniterpro@gmail.com>
2019-05-24 18:06:23 +0530

    changes

--- resources/views/front/press.blade.php
+++ resources/views/front/press.blade.php
@@ -10,25 +10,38 @@
     </div>
     <div class="press-content-section">
         <div class="press-wrapper">
-            @foreach($press['data'] as $presss)
-            <div class="blog-card">
-                <div class="blog-card-image">
-                    <img src="{{url('assets/img/press')}}/{{$presss['image']}}" alt="image not found" >
-                </div> 
-                <div class="blog-card-heading">{{$presss['title']}}</div>
-                <div class="blog-card-content" title="{{strip_tags($presss['description'])}}">{{strip_tags(str_limit(preg_replace("/&#?[a-z0-9]{2,8};/i","",$presss['description']),250))}}</div> 
-                <div class="link-wrapper">
-                    <a href="{{$presss['url']}}" target="_blank" class="read-more">Source</a>
-                </div>        
-            </div>
-            @endforeach
+            @if(!empty($press['data']))
+                @foreach($press['data'] as $presss)
+                <div class="blog-card">
+                    <div class="blog-card-image">
+                        <img src="{{url('assets/img/press')}}/{{$presss['image']}}" alt="image not found" >
+                    </div> 
+                    <div class="blog-card-heading">{{$presss['title']}}</div>
+                    <div class="blog-card-content" title="{{strip_tags($presss['description'])}}">{{strip_tags(str_limit(preg_replace("/&#?[a-z0-9]{2,8};/i","",$presss['description']),250))}}</div> 
+                    <div class="link-wrapper">
+                        <a href="{{$presss['url']}}" target="_blank" class="read-more">Source</a>
+                    </div>        
+                </div>
+                @endforeach
+
         </div>
     </div>
     <div class="pagination-links">
-        <a href="" class="links"><</a>
-        <a href="" class="links">1</a>
-        <a href="" class="links">2</a>
-        <a href="" class="links">3</a> 
-        <a href="" class="links">></a>
+        @if(!empty($_REQUEST['page']))
+            @if($_REQUEST['page']!=1)
+                <a href="{{$press['prev_page_url']}}" class="links forward"><</a>
+            @endif
+            @endif
+            @for($i=1;$i<=$press['last_page'];$i++)
+                <a href="{{$press['path'].'?page='.$i}}" class="links">{{$i}}</a>
+            @endfor
+        @if(!empty($_REQUEST['page']))
+            @if($_REQUEST['page']!=$press['last_page'])
+                <a href="{{$press['next_page_url']}}" class="links backward">></a>
+            @endif
+        @else
+                <a href="{{$press['next_page_url']}}" class="links backward">></a>
+        @endif
     </div>
+    @endif
 </div>
